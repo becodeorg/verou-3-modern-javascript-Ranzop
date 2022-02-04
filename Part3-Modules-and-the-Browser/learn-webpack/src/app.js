875025615532f2ef525a3cad98a26c1a69a59360
Angelgroenen <bm.bracke.michael@gmail.com>
2022-02-04 10:26:20 +0100

    Installed ESlint & prettier

--- Part3-Modules-and-the-Browser/learn-webpack/src/app.js
+++ Part3-Modules-and-the-Browser/learn-webpack/src/app.js
@@ -1,27 +1,44 @@
 import component from './component';
-import { DateTime } from "luxon";
-import createElement from "../../../modules/createElement.js";
+import {
+  DateTime
+} from 'luxon';
+import createElement from '../../../modules/createElement.js';
 import anime from 'animejs/lib/anime.es.js';
 
+let dateNow = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+
+setInterval(() => {
+  let dateNow = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+  
+  let DateNowPara = document.querySelector('p');
+  console.log(dateNow);
+  DateNowPara.innerHTML = dateNow;
+  }, 10000);
 
 
-
-const dt = DateTime.fromObject({day: 22, hour: 12}, {zone: 'America/Los_Angeles', numberingSystem: 'beng'});
+const dt = DateTime.fromObject({
+  day: 22,
+  hour: 12
+}, {
+  zone: 'America/Los_Angeles',
+  numberingSystem: 'beng'
+});
 console.log(dt);
 document.body.appendChild(component());
 
-const x = document.createElement("div");
+const x = document.createElement('div');
 document.body.appendChild(x);
 
-x.innerText = "yoyoyo"
+x.innerHTML = dateNow;
+
 
 anime({
-    targets: 'div',
-    translateX: 250,
-    rotate: '1turn',
-    backgroundColor: '#FFF',
-    duration: 800
-  });
+  targets: 'div',
+  translateX: 250,
+  rotate: '1turn',
+  backgroundColor: '#FFF',
+  duration: 800,
+});
 
 
 /* var V1 = Vector.create([3,4,5]);
@@ -36,4 +53,3 @@ console.log (c); */
 // c is the vector (15,45,-45)
 
 // npm run from the src file!!!
-
